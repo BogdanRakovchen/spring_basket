94bb9cf5c739e59e6f8e210aa0ab09dcc451c51a
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-09-09 22:10:19 +1000

    add change

--- src/main/java/pro/sky/spring_basket/BasketController.java
+++ src/main/java/pro/sky/spring_basket/BasketController.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+// rest
 @RequestMapping(path = "/order")
 @RestController
 public class BasketController {
