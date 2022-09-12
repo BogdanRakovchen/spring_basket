ee526b02d2c84b576b3bf266ddbdaeafb522b696
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-09-13 02:01:14 +1000

    add change

--- src/main/java/pro/sky/spring_basket/BasketServiceImp.java
+++ src/main/java/pro/sky/spring_basket/BasketServiceImp.java
@@ -1,26 +1,19 @@
 package pro.sky.spring_basket;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Service
-@Scope("prototype")
-public class BasketServiceImp implements BasketInterface {
+@SessionScope
+public class BasketServiceImp {
 
-    //Set<String> basket = new HashSet();
     List<Set> basketList = new ArrayList<>();
-    @Override
     public Boolean add(Set setList){
       return basketList.add(setList);
     };
-    @Override
     public List get() {
          return basketList;
     };
